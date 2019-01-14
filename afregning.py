6ad436441bfd8521cab5abdfc25ff63d971dd8a9
DanielAndreasen <dtan@netcompany.com>
2019-01-14 15:47:02 +0100

    Rename to all being AMOUNT

--- afregning.py
+++ afregning.py
@@ -3,8 +3,8 @@ from data import Data
 
 class Afregning(Data):
     def __init__(self, df):
-        self.df = df[['TRANSAKTIONSDATO', 'VIRKNINGSDATO', 'NYMFID', 'PAY_SEG_ID', 'CUR_AMT', 'FT_TYPE_FLG']]
+        self.df = df[['TRANSAKTIONSDATO', 'VIRKNINGSDATO', 'NYMFID', 'PAY_SEG_ID', 'AMOUNT', 'FT_TYPE_FLG']]
 
     @property
     def sum_amount(self):
-        return sum(self.df['CUR_AMT'])
+        return sum(self.df['AMOUNT'])
--- udtraek.py
+++ udtraek.py
@@ -4,9 +4,9 @@ from utils import is_integer
 
 class Udtraek(Data):
     def __init__(self, df):
-        self.df = df[['TransDTTM', 'EffectiveDate', 'NYMFID', 'Parent', 'Sibling_ID', 'Amount', 'FT_FLG']]
+        self.df = df[['TransDTTM', 'EffectiveDate', 'NYMFID', 'Parent', 'Sibling_ID', 'AMOUNT', 'FT_FLG']]
 
     @property
     def sum_amount(self):
         idx = [is_integer(a) for a in self.df['Parent']]
-        return sum(self.df.loc[idx, 'Amount'])
+        return sum(self.df.loc[idx, 'AMOUNT'])
