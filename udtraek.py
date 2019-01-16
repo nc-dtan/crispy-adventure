4b9c27fa98432d9b47840b23b90a8a41398f036c
DanielAndreasen <dtan@netcompany.com>
2019-01-16 11:15:04 +0100

    Extract data when date is given as a string

--- udtraek.py
+++ udtraek.py
@@ -1,5 +1,5 @@
 from data import Data
-from utils import is_integer
+from utils import is_integer, convert_from_str
 from datetime import datetime, timedelta
 
 
@@ -18,6 +18,11 @@ class Udtraek(Data):
         return self.df.TransDTTM[idx]
 
     def select_between_dates(self, start_date, end_date):
+        if isinstance(start_date, str):
+            start_date = convert_from_str(start_date)
+        if isinstance(end_date, str):
+            end_date = convert_from_str(end_date)
+
         mask = (start_date <= self.df['TransDTTM']) & (self.df['TransDTTM'] <= end_date)
         return Udtraek(self.df[mask])
 
@@ -30,6 +35,8 @@ class Udtraek(Data):
         return Udtraek(self.select_between_dates(start_date, end_date))
 
     def select_on_date(self, date):
+        if isinstance(date, str):
+            date = convert_from_str(date)
         start_date = datetime(date.year, date.month, date.day)
         end_date = start_date + timedelta(days=1)
         return Udtraek(self.select_between_dates(start_date, end_date))
