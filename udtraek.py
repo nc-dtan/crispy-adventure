6b73efce6a27dde4924981434f787819b87b1c0c
DanielAndreasen <dtan@netcompany.com>
2019-01-16 10:36:27 +0100

    Add methods to extract data given dates

--- udtraek.py
+++ udtraek.py
@@ -1,5 +1,6 @@
 from data import Data
 from utils import is_integer
+from datetime import datetime, timedelta
 
 
 class Udtraek(Data):
@@ -15,3 +16,20 @@ class Udtraek(Data):
     def duplicate_transDTTM(self):
         idx = self.df.TransDTTM.duplicated()
         return self.df.TransDTTM[idx]
+
+    def select_between_dates(self, start_date, end_date):
+        mask = (start_date <= self.df['TransDTTM']) & (self.df['TransDTTM'] <= end_date)
+        return self.df[mask]
+
+    def select_to_date(self, end_date):
+        start_date = self.df['TransDTTM'].values[0]  # Sorted after dates, so first date is start date
+        return self.select_between_dates(start_date, end_date)
+
+    def select_from_date(self, start_date):
+        end_date = self.df['TransDTTM'].values[-1]  # Sorted after dates, so last date is end date
+        return self.select_between_dates(start_date, end_date)
+
+    def select_on_date(self, date):
+        start_date = datetime(date.year, date.month, date.day)
+        end_date = start_date + timedelta(days=1)
+        return self.select_between_dates(start_date, end_date)
