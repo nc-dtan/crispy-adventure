1d135f1ef468c575f620d4141cf49a914b992092
nc-maen <maen@netcompany.com>
2019-01-22 10:57:42 +0100

    Load rpt data warehous files.

--- psrm_utils.py
+++ psrm_utils.py
@@ -1,6 +1,9 @@
 import os
 import pickle
+import pandas as pd
+import numpy
 from psrm_ci_ft_base import PSRM_CI_FT_BASE
+
 
 def cache_psrm(cache='psrm.pkl', force=False, *args, **kwargs):
     """Load and create cached psrm class instance.
@@ -14,6 +17,24 @@ def cache_psrm(cache='psrm.pkl', force=False, *args, **kwargs):
         with open(cache, 'rb') as f:
             psrm = pickle.load(f)
     return psrm
+
+
+def load_dw_rpt(path):
+    """Load weird rpt from DW export."""
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    # custom header for now
+    header = ['FordringID', 'FordringSaldo', 'InddrivelsesrenterAkk']
+    # delete weird rows
+    del lines[:2]
+    del lines[-3:]
+    arr = numpy.loadtxt(lines)
+    df = pd.DataFrame(arr, columns=header)
+    df['FordringID'] = df['FordringID'].astype('int64')
+    rename = {'FordringID': 'NYMFID'}
+    df.rename(columns=rename, inplace=True)
+    return df
+
 
 if __name__ == '__main__':
     from default_paths import path_v4, v4
