99970036a8f42f47d6514b16ead73b8c0da1f001
Emil Joachim Pedersen <ejp@netcompany.com>
2019-01-23 11:05:10 +0100

    Clean up and renaming

--- settlement_report.py
+++ settlement_report.py
@@ -10,12 +10,7 @@ from udtraek import Udtraek
 import os
 
 def settlement_id(nymfid):
-    #global temp
-    #idx = temp['NYMFID'] == nymfid
-    #print(len(temp))
     d = temp.loc[temp['NYMFID'] == nymfid, :]
-    #temp = temp.loc[~idx, :]
-    #Total_HF = sum_amount_code(d, code='DKHFEX')
     return {'NYMFID' : nymfid,
             'total_HF' : sum_amount_code(d, code='DKHFEX') + sum_amount_partial(d),
             'total_IR' : sum_amount_code(d)
@@ -57,9 +52,6 @@ else:
     time_start = time.time()
     with multiprocessing.Pool(processes = 8) as p:
         results = list(tqdm(p.imap(settlement_id, nymfids), total=len(nymfids)))
-    #with concurrent.futures.ThreadPoolExecutor() as executor:
-    #    results = list(tqdm(executor.map(settlement_id, nymfids), total=len(nymfids)))
-    #results = list(tqdm(map(settlement_id, nymfids), total=len(nymfids)))
     
     time_end = time.time()
     print("         ", time_end-time_start)
@@ -69,10 +61,6 @@ else:
 DW_settle = np.genfromtxt('../Data/DataV3/fordringsaldo.rpt', skip_header=2, skip_footer=2)
 DW_settle = pd.DataFrame(DW_settle, columns=['NYMFID', 'SALDO_HF','SALDO_RENTER'])
 DW_settle['NYMFID'] = DW_settle['NYMFID'].astype('int64')
-#DW_settle = DW_settle.sort_values(by=['NYMFID'])
-#DW_settle = DW_settle.reset_index(drop=True)
-#settlement = settlement.sort_values(by=['NYMFID'])
-#settlement = settlement.reset_index(drop=True)
 DW_settle.set_index('NYMFID', inplace=True)
 settlement.set_index('NYMFID', inplace=True)
 Settle_compare = DW_settle.join(settlement, on='NYMFID')
