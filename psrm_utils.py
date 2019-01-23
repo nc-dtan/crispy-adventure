c85db549208b4e21ef84b70bb3109597a41e04a3
nc-maen <maen@netcompany.com>
2019-01-23 12:12:44 +0100

    added total sum functions

--- psrm_utils.py
+++ psrm_utils.py
@@ -37,6 +37,23 @@ def load_dw_rpt(path, cache='fordringsaldo.pkl'):
     return df
 
 
+def und_udligning_total_split(nymfid, psrm):
+    """Get total udligning sum for NYMFID (underretning)."""
+    df = psrm.udligning.query('NYMFID == @nymfid')
+    df = df.query('Daekningstype != "FORDKORR"')
+    hf = df.query('DMIFordringTypeKategori == "HF"')
+    ir = df.query('DMIFordringTypeKategori != "HF"')
+    return hf['AMOUNT'].sum().round(2), ir['AMOUNT'].sum().round(2)
+
+
+def und_afregning_total_split(nymfid, psrm):
+    """Get total afregning sum for NYMFID (underretning)."""
+    df = psrm.underretning.query('NYMFID == @nymfid')
+    hf = df.query('DMIFordringTypeKategori == "HF"')
+    ir = df.query('DMIFordringTypeKategori != "HF"')
+    return hf['AMOUNT'].sum().round(2), ir['AMOUNT'].sum().round(2)
+
+
 if __name__ == '__main__':
     from default_paths import path_v4, v4
     psrm = cache_psrm(path=path_v4, input=v4)
