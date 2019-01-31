a1620d8995c49228549a79b45bb9c2117af08f34
Mathias Engel <maen@netcompany.com>
2019-01-31 22:34:07 +0100

    More refactor and DR report work.

---
+++ psrm/reports/dr.py
@@ -0,0 +1,93 @@
+import psrm
+from psrm.enums.fordringshaver import FordringsHaver as FH
+from tqdm import tqdm
+from report_utils import *
+
+
+# Load data
+ps = psrm.psrm_utils.cache_psrm(path=psrm.path_v4, input=psrm.v4)
+acl = annotate_acl(ps.udtraeksdata, claimant=FH.DR.value['Claimant_ID'])
+udl = annotate_udl(ps.udligning)
+afr = annotate_afr(ps.underretning)
+dr = saldo_DR()
+rm = saldo_PSRM()
+
+# Calculate totals of 'underretninger' on HF/~HF per NYMFID
+afr_hf = sum_NYMF(afr).rename(columns={'AMOUNT': 'AFR_HF'})
+afr_ir = sum_NYMF(afr, HF=False).rename(columns={'AMOUNT': 'AFR_IR'})
+udl_hf = sum_NYMF(udl).rename(columns={'AMOUNT': 'UDL_HF'})
+udl_ir = sum_NYMF(udl, HF=False).rename(columns={'AMOUNT': 'UDL_IR'})
+
+# Calculate debt, payments and interests per NYMFID
+debt = acl_debt(acl)
+pays = acl_pay(acl)
+intr = acl_interest(acl)
+
+# Create report, merge debt
+rep = pd.DataFrame(acl['NYMFID'].unique(), columns=['NYMFID'])
+rep = pd.merge(rep, debt, on='NYMFID', how='left')
+rep = pd.merge(rep, pays, on='NYMFID', how='left')
+rep = pd.merge(rep, intr, on='NYMFID', how='left')
+
+# Merge totals to report
+rep = pd.merge(rep, afr_hf, on='NYMFID', how='left')
+rep = pd.merge(rep, afr_ir, on='NYMFID', how='left')
+rep = pd.merge(rep, udl_hf, on='NYMFID', how='left')
+rep = pd.merge(rep, udl_ir, on='NYMFID', how='left')
+rep = rep.fillna(0)
+
+# Calculate remaining debt per NYMFID
+rep['DEBT_REMAIN'] = (rep['DEBT']-rep['PAYMENT']+rep['ACL_IR']).round(2)
+rep['ACL_HF'] = (rep['PAYMENT'] - rep['ACL_IR']).round(2)
+rep = pd.merge(rep, rm, on='NYMFID', how='left')
+assert rep['NYMFID'].is_unique
+assert len(rep) == len(acl['NYMFID'].unique())
+
+# Mark ballance errors
+rep['UDL_HF_OK'] = rep['ACL_HF'] == rep['UDL_HF']
+rep['AFR_HF_OK'] = rep['ACL_HF'] == rep['AFR_HF']
+rep['UDL_IR_OK'] = rep['ACL_IR'] == rep['UDL_IR']
+rep['AFR_IR_OK'] = rep['ACL_IR'] == rep['AFR_IR']
+ok = (rep['AFR_HF_OK'] & rep['AFR_IR_OK'] & rep['UDL_HF_OK'] & rep['UDL_IR_OK'])
+rep['ALL_OK'] = ok
+
+# Mark DB round error for 1 øre
+dr['Fejlkategori'] = False
+dr.loc[dr['Difference'] == -0.01, 'Fejlkategori'] = 'DB fejl'
+
+# Test for missing underretning
+for i, row in dr.iterrows():
+    if row['Fejlkategori'] != False:
+        continue
+    nymfid = row['NYMFID']
+    a = afr.query('NYMFID==@nymfid')
+    u = udl.query('NYMFID==@nymfid')
+    if len(a) == 0 and len(u) == 0:
+        dr.loc[dr['NYMFID']==nymfid, 'Fejlkategori'] = 'ingen aktivitet'
+        ac = acl.query('NYMFID==@nymfid')
+        if len(ac.query('ISPAY')) != 0:
+            raise ValueError
+dr = dr.set_index('NYMFID')
+rep = pd.merge(rep, dr, on='NYMFID', how='left')
+#rep.query('~ALL_OK & Saldo_DR.isnull()', inplace=True)
+rep.query('~ALL_OK', inplace=True)
+bool_sort = ['UDL_HF_OK', 'AFR_HF_OK','UDL_IR_OK','AFR_IR_OK']
+rep.sort_values(bool_sort, inplace=True)
+rep.to_csv('total_dr_test.csv', index=False)
+
+# samtidighed
+
+#rep = read_pickle('rep.pkl')
+#dr = dr.join(rep['ALL_OK'], how='left')
+#dr = dr.fillna(-1)
+#
+## Test for DR IR not included
+#for nymfid, row in dr[dr['Forklaring']==False].iterrows():
+#    a = afr.query('NYMFID==@nymfid')
+#    u = udl.query('NYMFID==@nymfid')
+#    ac = acl.query('NYMFID==@nymfid')
+#    debt = ac.query('ISDEBT')['AMOUNT'].sum().round(2)
+#    pay_hf = udl.query('NYMFID==@nymfid').query('ISHF')['AMOUNT'].sum().round(2)
+#    hr_remain = round(debt - pay_hf, 2)
+#    if row['Saldo_DR'] == hr_remain:
+#        dr.loc[nymfid, 'Forklaring'] = 'mangler renter'
---
+++ psrm/reports/report_utils.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from psrm.utils import utils
+from psrm.enums.justeringstype import JusteringsType as JT
+
+
+def annotate_acl(acl, end_date='2018-12-29', claimant=None):
+    """Timecut and annotate ACL-udtraek."""
+    end_date = pd.to_datetime(end_date)  # cutdate
+    acols = ['NYMFID', 'EFFECTIVE_DATE', 'PARENT_ID', 'AMOUNT', 'CLAIMANT_ID']
+    acl = acl[acols].copy()
+    acl['EFFECTIVE_DATE'] = pd.to_datetime(acl['EFFECTIVE_DATE'])
+    acl.query('EFFECTIVE_DATE < @end_date', inplace=True)
+    if claimant is not None:
+        acl.query('CLAIMANT_ID == @claimant', inplace=True)  # CLAMAINT
+    acl['ISDEBT'] = ((acl['PARENT_ID'] == JT.DKHFEX.value) |
+                     (acl['PARENT_ID'] == JT.DKOGEX.value) |
+                     (acl['PARENT_ID'] == JT.DKOREX.value) |
+                     (acl['PARENT_ID'] == JT.DKIGEX.value) )
+    acl['ISPAY'] = [utils.is_integer(x) for x in acl['PARENT_ID'].values]
+    acl['ISCATU'] = acl['PARENT_ID'] == JT.DKCSHACT.value
+    assert ~acl.isnull().sum().sum()  # check no nulls in acl
+    return acl
+
+
+def annotate_udl(udl, end_date='2018-12-29'):
+    """Prepare udligning underretning."""
+    end_date = pd.to_datetime(end_date)  # cutdate
+    udl = udl.copy()
+    udl['VIRKNINGSDATO'] = pd.to_datetime(udl['VIRKNINGSDATO'])
+    udl.query('VIRKNINGSDATO < @end_date', inplace=True)
+    udl.query('~Daekningstype.isnull()', inplace=True)
+    udl['ISHF'] = udl['DMIFordringTypeKategori'].isin(['IG', 'OG', 'HF', 'OR'])
+    return udl[['NYMFID', 'ISHF', 'AMOUNT', 'VIRKNINGSDATO']]
+
+
+def annotate_afr(afr):
+    """Prepare afregning underretning."""
+    afr = afr.copy()
+    afr.query('~Daekningstype.isnull()', inplace=True)
+    afr['ISHF'] = afr['DMIFordringTypeKategori'].isin(['IG', 'OG', 'HF', 'OR'])
+    return afr[['NYMFID', 'ISHF', 'AMOUNT']]
+
+
+def acl_debt(acl):
+    """Calculate the initial debt per NYMFID."""
+    debt = acl.query('ISDEBT').groupby('NYMFID')
+    debt = debt['AMOUNT'].sum().round(2).reset_index()
+    debt.rename(columns={'AMOUNT': 'DEBT'}, inplace=True)
+    debt.set_index('NYMFID', inplace=True)
+    return debt
+
+
+def acl_pay(acl):
+    """Calculate sum of all payments per NYMFID."""
+    pays = acl.query('ISPAY').groupby('NYMFID')
+    pays = pays['AMOUNT'].sum().round(2).reset_index()
+    pays.rename(columns={'AMOUNT': 'PAYMENT'}, inplace=True)
+    pays['PAYMENT'] = -pays['PAYMENT']
+    pays.set_index('NYMFID', inplace=True)
+    return pays
+
+
+def acl_interest(acl):
+    """Calculate sum of all interests on NYMFID."""
+    intr = acl.query('ISCATU').groupby('NYMFID')
+    intr = intr['AMOUNT'].sum().round(2).reset_index()
+    intr.rename(columns={'AMOUNT': 'ACL_IR'}, inplace=True)
+    intr['ACL_IR'] = -intr['ACL_IR']
+    intr.set_index('NYMFID', inplace=True)
+    return intr
+
+
+def sum_NYMF(df, HF=True):
+    """Calculate the total per NYMFID on HF/~HF."""
+    cond = 'ISHF'
+    if HF is not True:
+        cond = '~' + cond
+    gr = df.query(cond).groupby('NYMFID')
+    gr = gr['AMOUNT'].sum().round(2).reset_index()
+    return gr.set_index('NYMFID')
+
+
+def saldo_DR(path='../Data/Fordringer med diff iflg DR.xlsx'):
+    """Load DR's total data."""
+    dr = pd.read_excel(path)
+    del dr['Ventende_beløb_TS2']
+    del dr['Fakturanummer']
+    del dr['Saldo_PSRM']  # tested, same as Saldo in 20181231_exp_psrm....
+    dr['Saldo_DR'] = dr['Saldo_DR'].round(2)
+    dr['Difference'] = dr['Difference'].round(2)
+    dr.rename(columns={'Fordringsid': 'NYMFID'}, inplace=True)
+    assert dr['NYMFID'].is_unique
+    return dr
+
+
+def saldo_PSRM(path='../Data/20181231_exp_psrm_dr_afstemningsleverance.txt'):
+    """Load PSRM's total data."""
+    df = pd.read_csv(path, sep=';')
+    df['Saldo'] = df['Saldo'].round(2)
+    del df['HovedfordringID']
+    del df['FordringshaverReference']
+    del df['Udtraeksdato']
+    del df['#FordringshaverKode']
+    df.rename(columns={'FordringID': 'NYMFID'}, inplace=True)
+    return df
