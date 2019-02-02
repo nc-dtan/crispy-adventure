b1e453d66a3dc993e279400605c629dd89f0aa3c
Mathias Engel <maen@netcompany.com>
2019-02-02 01:17:28 +0100

    First working prototype.

--- webtool/app.py
+++ webtool/app.py
@@ -3,20 +3,36 @@ import pandas as pd
 import numpy as np
 import psrm
 app = Flask(__name__)
+   
+
+def _html(df):
+    return df.to_html(classes=['w3-table-all'], index=False)
+
 
 @app.route("/")
 def index():
-    import psrm
-
-    ps = psrm.psrm_utils.cache_psrm(path=psrm.path_v4, input=psrm.v4)
-    return render_template("index.html", ps=ps)
+    return render_template("index.html", df=None)
 
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    nymfid = request.form['nymfid']
+    nymfid = 100052710938
+    afregn, underret, udlign, udtraek = ps.get_by_id(nymfid)
+    cols = ['VIRKNINGSDATO', 'EFIBETALINGSIDENTIFIKATOR', 'AMOUNT',
+            'Daekningstype', 'DMIFordringTypeKategori']
+    afregn = afregn.df[['VIRKNINGSDATO', 'AMOUNT', 'FT_TYPE_FLG']]
+    underret = underret.df[cols]
+    udlign = udlign.df[cols]
+    udtraek = udtraek.df
+    del udtraek['NYMFID']
+    return render_template("index.html",
+                           afregn=_html(afregn),
+                           underret=_html(underret),
+                           udlign=_html(udlign),
+                           udtraek=_html(udtraek),)
+
 
 if __name__ == "__main__":
+    ps = psrm.psrm_utils.cache_psrm(path=psrm.path_v4, input=psrm.v4)
     app.run(debug=True)
