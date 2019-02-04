576453695f74202b75ca9f7f363c6277731e7804
Mathias Engel <maen@netcompany.com>
2019-02-04 16:24:25 +0100

    Ændret settlement så den bruger den nye utilslokation.

--- psrm/reports/settlement.py
+++ psrm/reports/settlement.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import psrm
 from psrm.utils import psrm_utils
-from report_utils import *
+from psrm import report_utils as ru
 
 
 # Assumptions
@@ -23,12 +23,12 @@ from report_utils import *
 # Load data
 ps = psrm.psrm_utils.cache_psrm(path=psrm.path_v4, input=psrm.v4)
 DR_ID = psrm.enums.fordringshaver.FordringsHaver.DR.value['Claimant_ID']
-acl = annotate_acl(ps.udtraeksdata, claimant=DR_ID)
-debt = acl_debt(acl)
-pays = acl_pay(acl)
-intr = acl_interest(acl)
-udl = annotate_udl(ps.udligning)
-afr = annotate_afr(ps.underretning)
+acl = ru.annotate_acl(ps.udtraeksdata, claimant=DR_ID)
+debt = ru.acl_debt(acl)
+pays = ru.acl_pay(acl)
+intr = ru.acl_interest(acl)
+udl = ru.annotate_udl(ps.udligning)
+afr = ru.annotate_afr(ps.underretning)
 
 # Create report - one row per NYMFID
 rep = pd.DataFrame(acl['NYMFID'].unique(), columns=['NYMFID'])
@@ -49,13 +49,13 @@ assert all(rep['PAYMENT'] >= 0)
 assert all(rep['INTEREST'] >= 0)
 
 # Calculate totals on HF/~HF per NYMFID
-udl_hf = sum_NYMF(udl).rename(columns={'AMOUNT': 'UDL_HF'})
+udl_hf = ru.sum_NYMF(udl).rename(columns={'AMOUNT': 'UDL_HF'})
 rep = pd.merge(rep, udl_hf, on='NYMFID', how='left')
-udl_ir = sum_NYMF(udl, HF=False).rename(columns={'AMOUNT': 'UDL_INTEREST'})
+udl_ir = ru.sum_NYMF(udl, HF=False).rename(columns={'AMOUNT': 'UDL_INTEREST'})
 rep = pd.merge(rep, udl_ir, on='NYMFID', how='left')
-afr_hf = sum_NYMF(afr).rename(columns={'AMOUNT': 'AFR_HF'})
+afr_hf = ru.sum_NYMF(afr).rename(columns={'AMOUNT': 'AFR_HF'})
 rep = pd.merge(rep, afr_hf, on='NYMFID', how='left')
-afr_ir = sum_NYMF(afr, HF=False).rename(columns={'AMOUNT': 'AFR_INTEREST'})
+afr_ir = ru.sum_NYMF(afr, HF=False).rename(columns={'AMOUNT': 'AFR_INTEREST'})
 rep = pd.merge(rep, afr_ir, on='NYMFID', how='left')
 
 rep = rep.fillna(0)
