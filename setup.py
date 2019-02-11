065ee6d6020251b18349803769dede73a3c31b65
Mathias Engel <maen@netcompany.com>
2019-02-11 12:33:03 +0100

    Added __init__.py at module level.

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup, find_packages
 from Cython.Distutils import build_ext
 
 
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+
 setup(
-	maintainer='ICI - Supernova',
+    maintainer='ICI - Supernova',
     name='psrm',
-	version='0.1',
-	long_description='',
-	license='',
+    version='0.1',
+    long_description='',
+    license='',
     packages=find_packages(),
     url='https://github.com/nc-dtan/crispy-adventure',
     cmdclass={'build_ext': build_ext},
 )
-
